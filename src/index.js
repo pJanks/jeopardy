fccc421be4cbd94aae68e7a6b6bf5f55ca3e5eb1
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-12-14 16:06:03 -0700

    Add import statements

--- src/index.js
+++ src/index.js
@@ -10,4 +10,8 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import '.Clue.js'
+
+import '../data.dataset.js'
+
 console.log('This is the JavaScript entry file - your code begins here.');
