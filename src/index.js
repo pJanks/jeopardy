8b1c17039584ef2da6e08abfa18ff8b33bcbf71a
Zachary Nemeroff <zanemeroff@gmail.com>
2019-12-14 18:35:01 -0700

    Modify exports

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import $ from 'jquery';
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-
-import '.Clue.js'
-
-import '../data.dataset.js'
+// import './images/turing-logo.png'
+//
+// import '.Clue.js'
+// 
+// import '../data.dataset.js'
 
 console.log('This is the JavaScript entry file - your code begins here.');
