08492c1a92fe8b4c75f12f18e284c5d5072b4462
Trond Makonese <trondation@gmail.com>
2019-12-16 16:55:25 -0700

    Started writing the test for for the Player class.

---
+++ test/player-test.js
@@ -0,0 +1,21 @@
+const chai = require("chai");
+const expect = chai.expect;
+let players;
+
+const Player = require("../src/Player");
+const playerData = require("../data/users");
+
+beforeEach(() => {
+  users = new Player(playerData[0]);
+})
+
+describe ('Users', () => {
+
+  it('should be a function', () => {
+    expect(Users).to.be.a('function');
+  })
+
+  it('should be an instance of a User', () => {
+    expect(players).to.be.an.instanceof(Player);
+  })
+});
