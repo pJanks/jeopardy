a634264acc1dbaea75d0e4adc51344c35bf5e426
Trond Makonese <trondation@gmail.com>
2019-12-16 16:56:22 -0700

    Minor corrections to the player test.

--- test/player-test.js
+++ test/player-test.js
@@ -6,13 +6,13 @@ const Player = require("../src/Player");
 const playerData = require("../data/users");
 
 beforeEach(() => {
-  users = new Player(playerData[0]);
+  players = new Player(playerData[0]);
 })
 
 describe ('Users', () => {
 
   it('should be a function', () => {
-    expect(Users).to.be.a('function');
+    expect(Player).to.be.a('function');
   })
 
   it('should be an instance of a User', () => {
