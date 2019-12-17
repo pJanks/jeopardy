023ec7d46bb430bb0551eab2ac1ed0d5c2cd99b4
Zachary Nemeroff <zanemeroff@gmail.com>
2019-12-16 17:58:09 -0700

    Update round categories

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -4,6 +4,9 @@ import Player from './Player.js'
 import Round from './Round.js'
 import Clue from './Clue.js'
 
+let game;
+let round1;
+
 const domUpdates = {
   printSingleQuestion: () => {
     $('.game-board').html(`
@@ -38,38 +41,49 @@ const domUpdates = {
 
   startGame: dataInfo => {
     let nums = domUpdates.shuffle();
-    const game = new Game(dataInfo.categories, dataInfo.clues, nums);
+    game = new Game(dataInfo.categories, dataInfo.clues, nums);
     const round1Nums = game.randomNumbers.splice(0, 4);
-    const round1 = new Round(round1Nums, game.roundNumber)
+    round1 = new Round(round1Nums, game.roundNumber);
     game.instanstiatePlayers($('.player1-input').val(), $('.player2-input').val(), $('.player3-input').val());
     $('.intro-container').addClass('hidden');
     $('.bottom').removeClass('hidden');
+
 
     $('.player-1-name').text($('.player1-input').val().toUpperCase())
     $('.player-2-name').text($('.player2-input').val().toUpperCase())
     $('.player-3-name').text($('.player3-input').val().toUpperCase())
 
     $('.game-board').html(`
-    <div class="board a1"><p>Catagory 1</p></div>
+    <div class="board a1"></div>
     <div class="board a2"><p class="number">100</p></div>
     <div class="board a3"><p class="number">200</p></div>
     <div class="board a4"><p class="number">300</p></div>
     <div class="board a5"><p class="number">400</p></div>
-    <div class="board b1"><p >Catagory 2</p></div>
+    <div class="board b1"></div>
     <div class="board b2"><p class="number">100</p></div>
     <div class="board b3"><p class="number">200</p></div>
     <div class="board b4"><p class="number">300</p></div>
     <div class="board b5"><p class="number">400</p></div>
-    <div class="board c1"><p>Catagory 3</p></div>
+    <div class="board c1"></div>
     <div class="board c2"><p class="number">100</p></div>
     <div class="board c3"><p class="number">200</p></div>
     <div class="board c4"><p class="number">300</p></div>
     <div class="board c5"><p class="number">400</p></div>
-    <div class="board d1"><p>Catagory 4</p></div>
+    <div class="board d1"></div>
     <div class="board d2"><p class="number">100</p></div>
     <div class="board d3"><p class="number">200</p></div>
     <div class="board d4"><p class="number">300</p></div>
     <div class="board d5"><p class="number">400</p></div>`);
+    domUpdates.assignRoundCategories();
+  },
+
+
+  assignRoundCategories: () => {
+    let keys = Object.keys(game.gameCategories)
+    $('.a1').text((keys[round1.categories[0]]).split(/(?=[A-Z])/).join(' ').toUpperCase())
+    $('.b1').text((keys[round1.categories[1]]).split(/(?=[A-Z])/).join(' ').toUpperCase())
+    $('.c1').text((keys[round1.categories[2]]).split(/(?=[A-Z])/).join(' ').toUpperCase())
+    $('.d1').text((keys[round1.categories[3]]).split(/(?=[A-Z])/).join(' ').toUpperCase())
   },
 
 
