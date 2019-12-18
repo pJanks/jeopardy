6969f6026405c88875de132b2eef287861e0a4fd
Trond240 <49410633+Trond240@users.noreply.github.com>
2019-12-18 16:08:01 -0700

    Round.js fixed

--- test/round-test.js
+++ test/round-test.js
@@ -1,21 +1,37 @@
 const chai = require("chai");
 const expect = chai.expect;
-import data from '../src/Round.js'
-let rounds;
+import Round from '../src/Round.js'
+import Clue from '../src/Clue.js'
 
-// const roundData = require("../data/users");
+describe ('Round', () => {
 
-// beforeEach(() => {
-//   rounds = new Round();
-// })
-//
-// describe ('Round', () => {
-//
-//   it('should be a function', () => {
-//     expect(Round).to.be.a('function');
-//   })
-//
-//   it('should be an instance of a User', () => {
-//     expect(rounds).to.be.an.instanceof(Round);
-//   })
-// });
+let round;
+let clue;
+
+beforeEach(() => {
+  clue = new Clue('Sound a dog makes', 'bark!!!', 10, 100);
+  round = new Round({'unitedStatesHistory': 1}, 1);
+})
+
+describe ('default properties', () => {
+
+
+  it('should be a function', () => {
+    expect(Round).to.be.a('function');
+  })
+
+  it('should be an instance of a Round', () => {
+    expect(round).to.be.an.instanceof(Round);
+  })
+
+  it('should have categories', () => {
+    expect(round.categories).to.deep.equal({'unitedStatesHistory': 1});
+  })
+
+  it('should have clues', () => {
+    round.clues.push(clue);
+    expect(round.clues).to.deep.equal([clue]);
+  })
+});
+});
+
