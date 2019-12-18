e4fe4fa94e93ffef2ce64225e5d1b5beeca2e672
Trond Makonese <trondation@gmail.com>
2019-12-18 14:06:47 -0700

    Refactor game-test.js.

--- test/game-test.js
+++ test/game-test.js
@@ -1,30 +1,59 @@
 const chai = require("chai");
 const expect = chai.expect;
-let games;
-
-const Game = require("../src/Game");
-// const playerData = require("../data/players");
-
-beforeEach(() => {
-  games = new Game(gameData[0]);
-})
+import Game from '../src/Clue'
 
 describe ('Game', () => {
+
+let game;
+let roundOne;
+let roundTwo;
+let roundThree;
+let playerOne;
+let playerTwo;
+let playerThree;
+
+beforeEach(() => {
+  games = new Game();
+  playerOne = new Player();
+  playerTwo = new Player();
+  playerThree = new Player();
+  roundOne = new Round();
+  roundTwo = new Round();
+  roundThree = new Round();
+
+})
 
   it.skip('should be a function', () => {
     expect(Game).to.be.a('function');
   })
 
   it.skip('should be an instance of a Game', () => {
-    expect(games).to.be.an.instanceof(Game);
+    expect(game).to.be.an.instanceof(Game);
   })
 
   it.skip('should be have game categories', () => {
-    expect(game.gameCategories).to.eqaul(categoriesData[0].gameCategories);
+    expect(game.gameCategories).to.eqaul({'unitedStatesHistory': 1});
   })
 
   it.skip('should begin round 1 when players are instantiated', () => {
-    expect(game.gameCategories).to.eqaul(categoriesData[0].gameCategories);
+    expect(game.players).to.eqaul(3);
+
+
   })
 
+  it.skip('should hold all clues', () => {
+    expect(game.allClues).to.eqaul();
+  })
+
+  it.skip('should show who the currebnt player is', () => {
+    expect(game.currentPlayer).to.eqaul();
+  })
+
+  it.skip('should show how many clues are remaining', () => {
+    expect(game.cluesRemaining).to.eqaul();
+  })
+
+  it.skip('', () => {
+    expect(game.randomNumbers).to.eqaul();
+  })
 });
