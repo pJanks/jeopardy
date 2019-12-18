ba13481293b41070009024e44d2191437a89d8d4
Trond Makonese <trondation@gmail.com>
2019-12-18 15:30:02 -0700

    Add player test.

--- test/player-test.js
+++ test/player-test.js
@@ -1,20 +1,46 @@
 const chai = require("chai");
 const expect = chai.expect;
+import Player from '../src/Player';
 let player;
 
-// const playerData = require("../data/players");
+describe ('Player', () => {
 
-// beforeEach(() => {
-//   player = new Player(playerData[0]);
-// })
-//
-// describe ('Player', () => {
-//
-//   it('should be a function', () => {
-//     expect(Player).to.be.a('function');
-//   })
-//
-//   it('should be an instance of a User', () => {
-//     expect(player).to.be.an.instanceof(Player);
-//   })
-// });
+
+beforeEach(() => {
+  player = new Player('Trond', 'bark!!!', 0);
+})
+
+describe ('default properties', () => {
+
+  it('should be a function', () => {
+    expect(Player).to.be.a('function');
+  })
+
+  it('should be an instance of a User', () => {
+    expect(player).to.be.an.instanceof(Player);
+  })
+
+  it('should have a name', () => {
+    expect(player.name).to.equal('Trond');
+  })
+
+  it('should be able to answer questions', () => {
+    expect(player.answer).to.equal('bark!!!');
+  })
+
+  describe ('update score', () => {
+
+  it('player.score should increase by 100', () => {
+    expect(player.score).to.equal(0);
+    player.updateScore(100);
+    expect(player.score).to.equal(100);
+  })
+
+  it('player.score should decrease score if answer is incorrect', () => {
+    expect(player.score).to.equal(0);
+    player.updateScore(-100);
+    expect(player.score).to.equal(-100);
+  })
+});
+});
+});
