273408d3f73e16b5b0e6e54eea61e1d8fb740b20
Trond Makonese <trondation@gmail.com>
2020-01-06 12:54:16 -0700

    Create spies for clue.checkAnswer method.

--- test/clue-test.js
+++ test/clue-test.js
@@ -41,5 +41,20 @@ describe ('default properties', () => {
     checkAnswer();
     expect().to.equal();
   })
+
+  describe ('spie on checkAnswer method', () => {
+    chai.spy.on(clue, ['checkAnswer'], () => true);
+
+    it('should be able to evaluate a wager and check player answer', () => {
+      evaluateWager('bark!!!');
+      expect(clue.checkAnswer).to.equal(true);
+    })
+
+    chai.spy.on(clue, ['checkAnswer'], () => false);
+
+    it('should be able to evaluate a wager and check player answer', () => {
+      evaluateWager('ruff ruff!!!');
+      expect(clue.checkAnswer).to.eqaul(false);
+});
 });
 });
